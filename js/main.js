3e5a3b15d7862b6bd31e76b5204c159ff7ea9b85
DESKTOP-TM6IV1U\mikos.zsamba <mikloszsamba@gmail.com>
2021-09-24 14:48:30 +0200

    minified js

--- js/main.js
+++ js/main.js
@@ -36,20 +36,7 @@ $(window).on( "scroll", function( event) {
 // mocked data
 let profilesArray = [
   { id: 1, src: "https://picsum.photos/id/4/300/300" },
-  { id: 2, src: "https://picsum.photos/id/5/300/300" },
-  { id: 3, src: "https://picsum.photos/id/6/300/300" },
-  { id: 4, src: "https://picsum.photos/id/7/300/300" },
-  { id: 5, src: "https://picsum.photos/id/8/300/300" },
-  { id: 6, src: "https://picsum.photos/id/9/300/300" },
-  { id: 7, src: "https://picsum.photos/id/10/300/300" },
-  { id: 8, src: "https://picsum.photos/id/11/300/300" },
-  { id: 9, src: "https://picsum.photos/id/12/300/300" },
-  { id: 10, src: "https://picsum.photos/id/13/300/300" },
-  { id: 11, src: "https://picsum.photos/id/14/300/300" },
-  { id: 12, src: "https://picsum.photos/id/15/300/300" },
-  { id: 13, src: "https://picsum.photos/id/16/300/300" },
-  { id: 14, src: "https://picsum.photos/id/17/300/300" },
-  { id: 15, src: "https://picsum.photos/id/18/300/300" }
+  { id: 2, src: "https://picsum.photos/id/5/300/300" }
 ];
 
 $(function () {
@@ -68,7 +55,3 @@ function populateProfilePictures(data) {
     $( "#profilePicContainer" ).append( div );
   }
 };
-
-function expandOurTeam() {
-  console.log(this);
-}
